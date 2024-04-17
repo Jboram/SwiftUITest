c3eb100bcf468775c80e8e249e064f5d80f26f26
jeong boram <jbram07@diavision.co.kr>
2024-04-17 13:34:51 +0900

    update designsystem

---
+++ SwiftUITest/Views/DesignSystem/CustomText.swift
@@ -0,0 +1,18 @@
+//
+//  CustomText.swift
+//  SwiftUITest
+//
+//  Created by diavision on 2024/04/16.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomText()
+}
